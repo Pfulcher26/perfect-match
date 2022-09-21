0419215ca7d6c85c693cde2b11d08ea4ef5d8bbb
pfulcher26 <pfulcher26@gmail.com>
2022-09-21 16:30:52 -0700

    Oauth troubles.

--- project_3/main_app/forms.py
+++ project_3/main_app/forms.py
@@ -2,6 +2,9 @@ from django.forms import ModelForm
 from .models import Skill
 from django.contrib.auth.forms import UserCreationForm
 from main_app.models import MyUser
+from django.utils.translation import gettext_lazy as _
+from django import forms
+
 
 class SkillForm(ModelForm):
     class Meta:
@@ -10,6 +13,9 @@ class SkillForm(ModelForm):
 
 class CustomUserCreationForm(UserCreationForm):
 
+    skills = forms.CharField(max_length=12, min_length=4, required=True, help_text='Required: skills',
+                         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Skills'}))
+
     class Meta(UserCreationForm.Meta):
         model = MyUser
-        fields = UserCreationForm.Meta.fields  
+        fields = UserCreationForm.Meta.fields + ('skills',)
