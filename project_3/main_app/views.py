83a86c244b50642da039f0fbe27759b517d978dd
pfulcher26 <pfulcher26@gmail.com>
2022-09-22 14:10:31 -0700

    Updates json request to retrieve web developer jobs.

--- project_3/main_app/views.py
+++ project_3/main_app/views.py
@@ -18,7 +18,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 import os
 
 #json that returns everything related to software engineering jobs 
-response = requests.get(f"https://api.adzuna.com/v1/api/jobs/us/search/1?app_id={os.environ['API_ID']}&app_key={os.environ['API_KEY']}&results_per_page=200&what=software")
+response = requests.get(f"https://api.adzuna.com/v1/api/jobs/us/search/1?app_id={os.environ['API_ID']}&app_key={os.environ['API_KEY']}&results_per_page=200&what=web_developer")
 job_list = []
 # from django.contrib.auth.backends import BaseBackend
 
